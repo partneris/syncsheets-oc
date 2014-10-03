e7e1910c70926d12e9c8558e19e8a14375c24952
Gaurav Mehra <gauravmehra1987@gmail.com>
2014-10-03 20:26:20 +0530

    entered the version info

--- admin/controller/feed/syncsheets.php
+++ admin/controller/feed/syncsheets.php
@@ -1,5 +1,5 @@
 <?php 
-define('GSS_VERSION', "0.2.6");
+define('GSS_VERSION', "0.2.7");
 class ControllerFeedSyncsheets extends Controller {
 	private $error = array(); 
         public $_log = array();
