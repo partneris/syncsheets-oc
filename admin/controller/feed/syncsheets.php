5c43777fbe2f754ba1499f7f6356269ddce1dc65
Gaurav Mehra <gauravmehra1987@gmail.com>
2015-02-11 17:57:00 +0530

    updator issue fixed

--- admin/controller/feed/syncsheets.php
+++ admin/controller/feed/syncsheets.php
@@ -1,5 +1,5 @@
 <?php 
-define('GSS_VERSION', "0.5.7");
+define('GSS_VERSION', "0.5.9");
 class ControllerFeedSyncsheets extends Controller {
 	private $error = array(); 
         public $_log = array();
