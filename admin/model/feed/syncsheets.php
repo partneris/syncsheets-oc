1cf6d5df90dd27f414866ad11356bddd24f18c59
Guntis Endzelis <guntis.e@gmail.com>
2015-03-20 18:05:24 +0200

    svetlanata reported issue with option not being generated. this is the fix

--- admin/model/feed/syncsheets.php
+++ admin/model/feed/syncsheets.php
@@ -1651,23 +1651,9 @@ class ModelFeedSyncsheets extends Model {
             }
         }
 
-        if (isset($data['product_option'])) {
-            foreach ($data['product_option'] as $product_option) {
-                if ($product_option['type'] == 'select' || $product_option['type'] == 'radio' || $product_option['type'] == 'checkbox' || $product_option['type'] == 'image') {
-                    $this->db->query("INSERT INTO " . DB_PREFIX . "product_option SET product_id = '" . (int) $product_id . "', option_id = '" . (int) $product_option['option_id'] . "', required = '" . (int) $product_option['required'] . "'");
-
-                    $product_option_id = $this->db->getLastId();
-
-                    if (isset($product_option['product_option_value']) && count($product_option['product_option_value']) > 0) {
-                        foreach ($product_option['product_option_value'] as $product_option_value) {
-                            $this->db->query("INSERT INTO " . DB_PREFIX . "product_option_value SET product_option_id = '" . (int) $product_option_id . "', product_id = '" . (int) $product_id . "', option_id = '" . (int) $product_option['option_id'] . "', option_value_id = '" . (int) $product_option_value['option_value_id'] . "', quantity = '" . (int) $product_option_value['quantity'] . "', subtract = '" . (int) $product_option_value['subtract'] . "', price = '" . (float) $product_option_value['price'] . "', price_prefix = '" . $this->db->escape($product_option_value['price_prefix']) . "', points = '" . (int) $product_option_value['points'] . "', points_prefix = '" . $this->db->escape($product_option_value['points_prefix']) . "', weight = '" . (float) $product_option_value['weight'] . "', weight_prefix = '" . $this->db->escape($product_option_value['weight_prefix']) . "'");
-                        }
-                    } else {
-                        $this->db->query("DELETE FROM " . DB_PREFIX . "product_option WHERE product_option_id = '" . $product_option_id . "'");
-                    }
-                } else {
-                    $this->db->query("INSERT INTO " . DB_PREFIX . "product_option SET product_id = '" . (int) $product_id . "', option_id = '" . (int) $product_option['option_id'] . "', option_value = '" . $this->db->escape($product_option['option_value']) . "', required = '" . (int) $product_option['required'] . "'");
-                }
+        if(isset($data['product_option'])){
+            foreach($data['product_option'] as $option){
+                $this->saveOption($option,$product_id);
             }
         }
 
